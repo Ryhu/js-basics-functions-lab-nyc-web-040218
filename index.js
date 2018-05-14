4cbe0080776760b9179b21c14c665adaed1e3408
Ryan Hu <ryanhuuu@gmail.com>
2018-05-14 18:20:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
-// Code your solution in this file!
+
+
+
+
+function distanceFromHqInBlocks(){
+  
+}
+// returns a distance in blocks ‣
+// ReferenceError: distanceFromHqInBlocks is not defined
+//     at Context.<anonymous> (test/indexTest.js:6:7)
+// returns a distance in blocks ‣
+// ReferenceError: distanceFromHqInBlocks is not defined
+//     at Context.<anonymous> (test/indexTest.js:10:7)
+// calculates distances below 42nd street ‣
+// ReferenceError: distanceFromHqInBlocks is not defined
+//     at Context.<anonymous> (test/indexTest.js:14:7)
+function distanceFromHqInFeet(){
+  
+}
+// returns a distance in feet ‣
+// ReferenceError: distanceFromHqInFeet is not defined
+//     at Context.<anonymous> (test/indexTest.js:20:7)
+// returns a distance in feet ‣
+// ReferenceError: distanceFromHqInFeet is not defined
+//     at Context.<anonymous> (test/indexTest.js:24:7)
+// calculates distances below 42nd street ‣
+// ReferenceError: distanceFromHqInFeet is not defined
+//     at Context.<anonymous> (test/indexTest.js:28:7)
+function distanceTravelledInFeet(){
+  
+}
+// returns the distance travelled in feet ‣
+// ReferenceError: distanceTravelledInFeet is not defined
+//     at Context.<anonymous> (test/indexTest.js:34:7)
+// returns a distance in feet ‣
+// ReferenceError: distanceTravelledInFeet is not defined
+//     at Context.<anonymous> (test/indexTest.js:38:7)
+// returns distance when destination is below distance ‣
+// ReferenceError: distanceTravelledInFeet is not defined
+//     at Context.<anonymous> (test/indexTest.js:42:7)
+// calculatesFarePrice(start, destination)
+// gives customers a free sample ‣
+// ReferenceError: calculatesFarePrice is not defined
+//     at Context.<anonymous> (test/indexTest.js:48:7)
+// charges 2 cents per foot when total feet travelled is between 400 and 2000 (remember the first 400 feet are free!) ‣
+// ReferenceError: calculatesFarePrice is not defined
+//     at Context.<anonymous> (test/indexTest.js:53:7)
+// charges 25 dollars for a distance over 2000 feet ‣
+// ReferenceError: calculatesFarePrice is not defined
+//     at Context.<anonymous> (test/indexTest.js:57:7)
+// does not allow rides over 2500 feet ‣
+// ReferenceError: calculatesFarePrice is not defined
+//     at Context.<anonymous> (test/indexTest.js:61:7)
